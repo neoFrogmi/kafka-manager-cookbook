08eacc12b9681fad73c7199ca4f915bc68c10c66
Tomas Martišius <tomas.martisius@getcujo.com>
2017-11-15 12:40:20 +0200

    Fix cookbook to support systemd.

--- recipes/default.rb
+++ recipes/default.rb
@@ -17,6 +17,35 @@ template '/usr/share/kafka-manager/conf/application.conf' do
   notifies :restart, 'service[kafka-manager]'
 end
 
+if (node['platform'] == 'ubuntu' && node['platform_version'].to_f > 16.0) || (node['platform'] == 'debian' && node['platform_version'].to_f > 9.0)
+   template "systemd-service" do
+     path "/lib/systemd/system/kafka-manager.service"
+     source "systemd-service.erb"
+     owner "root"
+     group "root"
+     mode "0644"
+     variables(
+         :service_name => 'kafka-manager',
+         :jar_path => '/usr/share/kafka-manager/bin/kafka-manager',
+         :service_user => 'kafka-manager',
+         :service_group => 'kafka-manager'
+     )
+   end
+ end
+
+execute "systemctl daemon-reload" do
+  command "systemctl daemon-reload"
+  action :nothing
+  subscribes :run, 'remote_file[download_jar]', :immediately
+  only_if { (node['platform'] == 'ubuntu' && node['platform_version'].to_f > 16.0) || (node['platform'] == 'debian' && node['platform_version'].to_f > 9.0) }
+end
+
+execute "systemctl enable service" do
+  command "systemctl enable kafka-manager.service"
+  action :run
+  only_if { (node['platform'] == 'ubuntu' && node['platform_version'].to_f > 16.0) || (node['platform'] == 'debian' && node['platform_version'].to_f > 9.0) }
+end
+ 
 service 'kafka-manager' do
   action [ :enable, :start ]
 end
